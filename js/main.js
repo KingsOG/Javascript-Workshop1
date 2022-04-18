39f445c4a67bfd43229eed5eee10bbfbd03d08d3
Kingsley Ogbonna <kingsog@Kingsleys-MacBook-Air.local>
2022-04-18 22:59:07 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+//Javascript course... Console logs, Datas. Arithmetics
+console.log(30);
+console.log(40 + 2); // Print 42 to console.
+console.log(2 - 1); // Prints 1 to the console.
+console.log(50 / 5); // Prints 10 to the console.
+console.log("I love to code"); // Prints I love to code to the console.
+
+//Concatenation = The process of appending one string to the other
+
+console.log("front " + "space"); //Prints front space
+console.log("middle" + " " + "space"); // Prints middle space
+console.log("no"+"space"); //Prints nospace
+
+/*Inside a console.log() statement, concatenate the two strings 'Hello' and 'World'.
+
+Note: You should concatenate the two strings exactly (without introducing any additional characters).*/
+
+console.log("Hello" + "World");
+
+/*We left off the space last time. Create a second console.log() statement in which you concatenate the strings 'Hello' and 'World', but this time make sure to also include a space (' ') between the two words.*/
+
+console.log("Hello" + " " + "World");
+
+//Use the .length property to log the number of characters in the following string to the console:
+
+console.log("Teaching the world how to code".lenght);
+
+
