ca46c5f3e8a928675193eb85e219f3d2e81c75d1
Kingsley Ogbonna <kingsog@Kingsleys-MacBook-Air.local>
2022-04-19 03:25:01 +0100

    add new course work exercises

--- js/main.js
+++ js/main.js
@@ -25,4 +25,28 @@ console.log("Hello" + " " + "World");
 
 console.log("Teaching the world how to code".lenght);
 
+// Use .toUpperCase() to log 'Codecademy' in all uppercase letters
+console.log("Codecademy".toUpperCase());
 
+// Use a string method to log the following string without whitespace at the beginning and end of it.
+console.log("  remove whitespace  ".trim());
+
+//Built-in Objects
+//Inside of a console.log(), create a random number with Math.random(), then multiply it by 100.
+console.log(Math.random()*100);
+
+//Now, use Math.floor() to make the output a whole number
+
+console.log(Math.floor(Math.random()*100)); //77
+
+/*Find a method on the JavaScript Math object that returns the smallest integer greater than or equal to a decimal number.
+
+Use this method with the number 43.8. Log the answer to the console.*/
+
+console.log(Math.ceil(43.8)); //44
+
+/*Use the JavaScript documentation to find a method on the built-in Number object that checks if a number is an integer.
+
+Put the number 2017 in the parentheses of the method and use console.log() to print the result.*/
+
+console.log(Number.isInteger(2017)); //True
